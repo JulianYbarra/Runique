35c15dba34a6245f82bbc3d3bd52b9fc371c87a1
JulianYbarra <julianybarra95@gmail.com>
2024-05-14 20:30:33 -0300

    add error utils

---
+++ core/domain/src/main/java/com/junka/core/domain/util/Result.kt
@@ -0,0 +1,21 @@
+package com.junka.core.domain.util
+
+
+sealed interface Result<out D, out E : Error> {
+    data class Success<out D>(val data: D) : Result<D, Nothing>
+
+    data class Failure<out E : Error>(val error: E) : Result<Nothing, E>
+}
+
+inline fun <T, E : Error, R> Result<T, E>.map(transform: (T) -> R): Result<R, E> {
+    return when (this) {
+        is Result.Success -> Result.Success(transform(data))
+        is Result.Failure -> Result.Failure(error)
+    }
+}
+
+fun <T, E : Error> Result<T,E>.asEmptyDataResult() : EmptyDataResult<E>{
+    return map {  }
+}
+
+typealias EmptyDataResult<E> = Result<Unit, E>
